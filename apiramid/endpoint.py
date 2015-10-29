0281a50a56686d91721d15d4cbac39dad53e5638
sinoroc <sinoroc@users.noreply.github.com>
2015-10-29 19:16:59 +0100

    Move decorators for endpoint to their own module

---
+++ apiramid/endpoint.py
@@ -0,0 +1,196 @@
+""" Endpoint module
+"""
+
+
+import inspect
+import logging
+import pyramid
+import venusian
+
+from . import api
+from . import i18n
+from . import util
+from . import view
+
+
+MODULE_NAME = __name__
+
+_ = i18n._
+
+LOG = logging.getLogger(__name__)
+
+
+class EndpointDefaults(object):
+    """ Class decorator.
+        Set defaults for methods decorated with ``Endpoint`` decorator.
+    """
+
+    ATTRIBUTE_NAME = '{}_endpoint_defaults'.format(MODULE_NAME)
+
+    def __init__(self, **kwargs):
+        """ Expected arguments: See Endpoint.__init__
+        """
+        self.endpoint_defaults = kwargs
+        return None
+
+    def __call__(self, decoratee):
+        setattr(decoratee, self.ATTRIBUTE_NAME, self.endpoint_defaults)
+        result = decoratee
+        return result
+
+
+class Endpoint(object):
+    """ Decorator for function, method or class callable.
+    """
+
+    VENUSIAN_CATEGORY = MODULE_NAME
+    VENUSIAN_SUBCATGEORY = MODULE_NAME
+
+    routes = []
+
+    def __init__(self, **kwargs):
+        """ Expected keywords:
+            ``method``
+            ``path``
+        """
+        self.endpoint = kwargs
+        # utility members
+        self.api_util = None
+        self.decoratee = None
+        self.decoratee_class = None
+        self.resource = None  # resource parsed from the API document
+        self.venusian_info = None
+        return None
+
+    def __call__(self, decoratee):
+        self.decoratee = decoratee
+        self.venusian_info = venusian.attach(
+            decoratee,
+            self.callback,
+            category=self.VENUSIAN_CATEGORY,
+            name=self.VENUSIAN_SUBCATGEORY,
+        )
+        return decoratee
+
+    def callback(self, scanner, dummy_name, obj):
+        """ Venusian callback.
+        """
+        if self.venusian_info.scope == 'class':
+            self.decoratee_class = obj
+        config = scanner.config.with_package(self.venusian_info.module)
+        self.set_defaults(getattr(obj, EndpointDefaults.ATTRIBUTE_NAME, {}))
+        self.check()
+        self.api_util = config.registry.queryUtility(api.IApi)
+        resource = self.api_util.find_resource(
+            self.endpoint['path'],
+            self.endpoint['method'],
+        )
+        if resource:
+            self.resource = resource
+            self.add_endpoint(config)
+        else:
+            LOG.warning(_("Could not find resource."))
+        return None
+
+    def set_defaults(self, defaults):
+        """ Set defaults from ``EntrypointDefaults`` if any.
+            Already set values are not overwritten of course.
+        """
+        for key, value in defaults.items():
+            if key not in self.endpoint:
+                self.endpoint[key] = value
+        return None
+
+    @property
+    def name_route(self):
+        """ Name of the route for this endpoint.
+        """
+        result = '{prefix}_{path}'.format(
+            prefix=MODULE_NAME,
+            path=self.endpoint['path'],
+        )
+        return result
+
+    def check(self):
+        """ Check the endpoint arguements.
+        """
+        if 'method' not in self.endpoint:
+            comment = _("Endpoint has no method {}").format(self.endpoint)
+            raise ValueError(comment)
+        if 'path' not in self.endpoint:
+            comment = _("Endpoint has no path {}").format(self.endpoint)
+            raise ValueError(comment)
+        return None
+
+    def add_endpoint(self, config):
+        """ Set an endpoint: add route if not available yet and add view.
+        """
+        self.add_route(config)
+        self.add_view(config)
+        return None
+
+    def add_route(self, config):
+        """ Add a Pyramid route if not added yet.
+        """
+        name = self.name_route
+        if name not in self.routes:
+            self.routes.append(name)
+            path = '{base_path}{rel_path}'.format(
+                base_path=self.api_util.base_path,
+                rel_path=self.endpoint['path'],
+            )
+            config.add_route(name, path)
+            self.add_options_view(config)
+        return None
+
+    def add_view(self, config):
+        """ Add a Pyramid view.
+        """
+        config.add_view(
+            view=self.view_callable,
+            route_name=self.name_route,
+            request_method=self.endpoint['method'],
+        )
+        return None
+
+    def add_options_view(self, config):
+        """ Add the Pyramid view for the OPTIONS method.
+        """
+        config.add_view(
+            view=util.options_view,
+            route_name=self.name_route,
+            request_method='OPTIONS',
+        )
+        return None
+
+    def view_callable(self, *args, **kwargs):
+        """ The view callable.
+        """
+        request = args[1]
+        view.checkin(request, self)
+        request.response = self.run_user_view_callable(*args, **kwargs)
+        view.checkout(request, self)
+        result = view.render(request)
+        return result
+
+    def run_user_view_callable(self, *args, **kwargs):
+        """ Run the actual view callable decorated in the user application.
+        """
+        result = None
+        if self.venusian_info.scope == 'class':
+            # decoratee is a method
+            view_callable = self.decoratee_class(*args, **kwargs)
+            result = self.decoratee(view_callable)
+        elif inspect.isclass(self.decoratee):
+            # decoratee is a class callable
+            view_callable = self.decoratee(*args, **kwargs)
+            result = view_callable()
+        elif inspect.isfunction(self.decoratee):
+            # decoratee is a function
+            result = self.decoratee(*args, **kwargs)
+        else:
+            raise Exception(_("Could not identify view callable."))
+        return result
+
+
+# EOF
